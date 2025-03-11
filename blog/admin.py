721c6c592c41af72a04ea3143875d44a40e218f9
Vilen <v.avtandilyan@mail.ru>
2025-03-11 14:44:47 +0300

    Создание функционала сайта, добавление данных в базу данных

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category, Location
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'text', 'pub_date',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'description',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+admin.site.empty_value_display = 'Не задано'
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
